12f3fca55a1836588bea107a1c36dc7d8ef49fee
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-11-11 19:04:24 +0530

    updates

--- youtube-vid/src/Components/Profile.js
+++ youtube-vid/src/Components/Profile.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Profile.css'
 import { useNavigate } from 'react-router-dom'
 
 function Profile() {
     const router = useNavigate();
+
+    const [royal, setRoyal] = useState([
+      {
+          image: "https://i.ytimg.com/vi/RLzC55ai0eo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtSyHZtzUbm1sUzuDS7Fw3tpalxw",
+          name: "Heeriye (Official Video) jasleen Royal ft",
+          singer: "Jasleen Royal",
+          views: "191M views",
+          months: "3 months ago"
+      },
+      {
+          image: "https://i.ytimg.com/vi/szvt1vD0Uug/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDtm2ZCQXp2wBZPW0-SWdhcRdTgaA",
+          name: "Leo-Na Ready lyric video | Thalapathy",
+          singer: "Sony Music South",
+          views: "175M views",
+          months: "4 months ago"
+      },{
+          image: "https://i.ytimg.com/vi/HrnrqYxYrbk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCH_xtr8WZFOKYc1PDBC7wwj6tPwQ",
+          name: "ANIMAL: SATRANGA | Ranbir kapoor",
+          singer: "T-Series",
+          views: "35M views",
+          months: "6 days ago"
+      },
+      {
+          image: "https://i.ytimg.com/vi/vgm1u2gPxzw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAANERxsJfA9W22Vlxd2rpZFFjx7A",
+          name: "Ranjhanaah Hua Mai Tera (video song)",
+          singer: "Eros Now Music",
+          views: "96M views",
+          months: "10 years ago"
+      },
+      {
+          image: "https://i.ytimg.com/vi/0Olv8zM_rps/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCqBs17KZIXLr51AhzPJzXBBEiHAw",
+          name: "Darling-Neeve Video | Prabhas| G.V.",
+          singer: "SonyMusicSouthVEVO",
+          views: "28M views",
+          months: "8 years ago"
+      },
+      {
+          image: "https://i.ytimg.com/vi/pA2twF9tWBw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBvTa9BEkBoH6ADuOAiWx8VBqGYZQ",
+          name: "Suttamla Soosi Lyrical Video | Gangs of ",
+          singer: "Aditya Music India",
+          views: "17M views",
+          months: "2 months ago"
+      },
+      {
+          image: "https://i.ytimg.com/vi/2pgx-tajxwE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC1dOsK9RVrmQT4thKRu4agC2Gjaw",
+          name: "Nijame Ne Chebuthuna Lyrical |",
+          singer: "Aditya Music India",
+          views: "58M views",
+          months: "7 months ago"
+      },
+      {
+          image: "https://i.ytimg.com/vi/VzT2xurZrbI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCpsEAXdu3WI_-qH8qFMAhklcDpZg",
+          name: "King of Kotha - Kalapakkaara video",
+          singer: "Sony Music South",
+          views: "30M views",
+          months: "2 months ago"
+      },
+      {
+          image: "https://i.ytimg.com/vi/MsGaOJOTQSA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACzvKHUDVCfHylxczeukjh4JSCzw",
+          name: "Nava Manmadhudu - Manasa Manasa",
+          singer: "SonyMusicSouthVEVO",
+          views: "943k views",
+          months: "6 years ago"
+      },
+      {
+          image: "https://i.ytimg.com/vi/bgIemsDOIK4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBBDVjvl8zyY_OliQ4RMzHL1kPdYQ",
+          name: "Nee Chuttu Chuttu - video song",
+          singer: "Junglee Music Telugu",
+          views: "1.2M views",
+          months: "6 days ago"
+      }
+  ])
   return (
     <>
        <div className='navbar20'>
@@ -160,6 +232,31 @@ function Profile() {
                             </button>
                         </div>
                     </div>
+                </div>
+
+                <div className='profile6'>
+                  <i class="fa-solid fa-clock-rotate-left" style={{color: "#e8eaee"}}></i>
+                  <h2>History</h2>
+                  <p>See all</p>
+                </div>
+
+
+                <div style={{display: "flex", flexWrap: "wrap", justifyContent: "space-between", height: "120px", marginTop: "20px"}}>
+                    {royal.map((pro) => (
+                        <div style={{width: "100%", display: "flex", justifyContent: "space-between", height: "140px"}}>
+                        <div style={{width: "48%", height: "120px"}}>
+                             <img style={{width: "100%", height: "100%", borderRadius: "10px"}} src={pro.image}/>
+                        </div>
+                        <div style={{width: "48%", height: "120px", color: "white"}}>
+                            <h2 style={{fontSize: "16px", fontWeight: "bold", textAlign: "left"}}>{pro.name}</h2>
+                            <p style={{textAlign: "left", marginTop: "-5px", fontSize: "14px"}}>{pro.singer}</p>
+                            <div style={{display: "flex", marginTop: "-17px", fontSize: "14px"}}>
+                                <p>{pro.views}</p>
+                                <p>{pro.months}</p>
+                            </div>
+                        </div>
+                        </div>
+                    ))}
                 </div>
             </div>
           </div>
