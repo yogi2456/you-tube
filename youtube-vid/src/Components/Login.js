46b1ae7f51161e0fa079418ffeb3c937a1ed2c81
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-12-05 14:17:21 +0530

    updates

--- youtube-vid/src/Components/Login.js
+++ youtube-vid/src/Components/Login.js
@@ -1,49 +1,81 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import './Login.css'
+import toast from 'react-hot-toast';
+// import axios from 'axios';
 
-function Login() {
+const Login = () => {
 
-  const router = useNavigate();
+    const router = useNavigate();
+
+    const [userData, setUserData] = useState({email: "", password: ""});
+
+    const handleChange = (event) => {
+        setUserData({...userData, [event.target.name]: event.target.value})
+    }
+
+    const handleForm = async (event) => {
+        event.preventDefault();
+        if(userData.email && userData.password) {
+           if(userData.password.length >= 8) {
+            try {
+                // const response = await axios.post("http://localhost:8000/api/v2/auth/register", { userData });
+                // if(response.data.success) {
+                //     toast.success("Account created successfully")
+                //     setUserData({ email: "", password: ""})
+                //     router('/login')
+                
+                const response = { data: {success: true}};
+                if(response.data.success) {
+                    alert("login success")
+                    setUserData({ email: '', password: ''});
+                    router('/')
+                }
+                // } else {
+                //     throw new Error("Something went wrong...")
+                //   }
+            } catch (error) {
+                toast.error(error?.message)
+                console.log(error, "error here")
+            }
+           } else {
+            toast.error("password should be 8 digits")
+           }
+        } else {
+            toast.error("please fill all the values")
+        }
+    }
+
   return (
-    <div>
-      <div className='login'>
+    <>
+    <div className='login'>
         <div className='login1'>
             <div className='login2'>
             <h2>Sign in</h2>
-            <p>to continue to yotube</p>
-            <div className='login3'>
-                <input placeholder='Email or phone'/>
-            </div>
-            <p className='login4'>Forget email?</p>
-            <div className='login5'>
-                <input placeholder='Password'/>
-            </div>
-            <p className='login4'>Forget Password?</p>
-            <div className='login6'>
-                <p>Not your computer? Use Guest mode to sign in privately.</p>
-                <p>Learn more</p>
-            </div>
-            <div className='login7'>
-                <p onClick={ () => router(`/register`)}>Create account</p>
-                <button onClick={() => router(`/`)}>Next</button>
-            </div>
+            <p>to continue to YouTube</p>
+
+            <form onSubmit={handleForm}>
+            <input className='login5' placeholder='Email or phone' name='email' type='email' onChange={handleChange}/> <br/>
+            <input className='login6' placeholder='Password' name='password' type='password' onChange={handleChange}/> <br/>
+            <button className='login3' onClick={() => router('/register')}>Create account</button>
+            <button className='login10' type='submit'>Login</button>
+            </form>
             </div>
         </div>
 
-        <div className='login8'>
-            <div className='login9'>
+        <div className='login7'>
+            <div className='login8'>
                 <p>English (United States)</p>
                 <p><i class="fa-solid fa-caret-down"></i></p>
             </div>
-            <div className='login10'>
+            <div className='login9'>
                 <p>Help</p>
                 <p>Privacy</p>
                 <p>Terms</p>
             </div>
         </div>
-      </div>
-    </div>
+    </div>    
+    </>
   )
 }
 
--- youtube-vid/src/Components/Register.js
+++ youtube-vid/src/Components/Register.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Register.css'
+import toast from 'react-hot-toast';
+import axios from 'axios';
 
 const Register = () => {
 
-  const router = useNavigate();
+    const router = useNavigate();
+
+    const [userData, setUserData] = useState({name: "", lastname: "", email: "", password: ""});
+
+    const handleChange = (event) => {
+        setUserData({...userData, [event.target.name]: event.target.value})
+    }
+
+    const handleForm = async (event) => {
+        event.preventDefault();
+        if(userData.name && userData.lastname && userData.email && userData.password) {
+           if(userData.password.length >= 8) {
+            try {
+                // const response = await axios.post("http://localhost:8000/api/v2/auth/register", { userData });
+                // if(response.data.success) {
+                //     toast.success("Account created successfully")
+                //     setUserData({name: "", lastname: "", email: "", password: ""})
+                //     router('/login')
+                
+                const response = { data: {success: true}};
+                if(response.data.success) {
+                    alert("register success")
+                    setUserData({name: '', email: '', password: ''});
+                    router('/login')
+                }
+                // } else {
+                //     throw new Error("Something went wrong...")
+                //   }
+            } catch (error) {
+                toast.error(error?.message)
+                console.log(error, "error here")
+            }
+           } else {
+            toast.error("password should be 8 digits")
+           }
+        } else {
+            toast.error("please fill all the values")
+        }
+    }
+
   return (
     <>
     <div className='register'>
@@ -12,12 +53,12 @@ const Register = () => {
             <div className='register2'>
             <h2>Create a Google Account</h2>
             <p>Enter your name</p>
-            <form>
-            <input className='register3' placeholder='First name'/> <br/>
-            <input className='register4' placeholder='Last name (Optional)'/> <br/>
-            <input className='register5' placeholder='Email or phone'/> <br/>
-            <input className='register6' placeholder='Password'/> <br/>
-            <button className='register10' onClick={ () => router(`/login`)}>Create</button>
+            <form onSubmit={handleForm}>
+            <input className='register3' placeholder='First name' name='name' type='text' onChange={handleChange}/> <br/>
+            <input className='register4' placeholder='Last name (Optional)' name='lastname' type='text' onChange={handleChange}/> <br/>
+            <input className='register5' placeholder='Email or phone' name='email' type='email' onChange={handleChange}/> <br/>
+            <input className='register6' placeholder='Password' name='password' type='password' onChange={handleChange}/> <br/>
+            <button className='register10' type='submit'>Sign in</button>
             </form>
             </div>
         </div>
