c38c297c0e7e6fe7b79114e6f77f7d1965380414
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-12-06 17:48:09 +0530

    updates

--- youtube-vid/src/helpers/AxiosConfig.js
+++ youtube-vid/src/helpers/AxiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-var BackendUrl = "http://localhost:8000/api/v2"
+var BackendUrl = "http://localhost:8000/api/v1"
 if (process.env.MODE === "PRODUCTION") {
-    BackendUrl = "https://awdiz-backend-4.onrender.com/api/v2"
+    BackendUrl = "https://awdiz-backend-4.onrender.com/api/v1"
 }
 
 const api = axios.create({ baseURL: BackendUrl })
